96fc2671b83c46d22dfacb55cb2804196b9e074e
Jp <joaopedrolimasaar@hotmail.com>
2023-06-11 17:47:05 -0300

    melhoria

--- handlerGet.go
+++ handlerGet.go
@@ -15,35 +15,36 @@ func handlerGet(responseWriter http.ResponseWriter, request *http.Request) {
 
 	db, err := sql.Open("postgres", connStr)
 	defer db.Close()
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(request.URL.RawQuery)
 	id := 1
-	resultSet, err := db.Query("SELECT acount_name FROM accounts WHERE id = $1", id)
+	rows, err := db.Query("SELECT acount_name FROM accounts WHERE id = $1", id)
+	defer rows.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resultSet.Close()
 
 	var name string
 	var quant int16
-	for resultSet.Next() {
-		if err := resultSet.Scan(&name); err != nil {
+	for rows.Next() {
+		if err := rows.Scan(&name); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Printf("Name of the id:%d is %s\n", id, name)
 		quant++
 	}
 
-	if quant == 0 {
-		fmt.Printf("Not Found for the id %d", id)
-	}
-
 	type user struct {
 		Mensagem string `json:"msg"`
+	}
+
+	if quant == 0 {
+		msg := fmt.Sprintf("Not Found for the id %d", id)
+		fmt.Printf(msg, id)
+		respondWithJson(responseWriter, 404, user{Mensagem: msg})
 	}
 
 	respondWithJson(responseWriter, 200, user{Mensagem: name})
